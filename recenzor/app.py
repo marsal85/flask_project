6f8b192dda576ea30474e17d601e11418b4416f4
Marsal <saligaaa@gmail.com>
2020-08-17 11:28:38 +0000

    Initial application

---
+++ recenzor/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import render_template
+
+flask_app = Flask(__name__)
+
+@flask_app.route('/')
+def view_welcome_page():
+    return render_template("welcome_page.jinja")
+	
+@flask_app.route('/about/')
+def view_about():
+    return render_template("about.jinja")
---
+++ run.py
@@ -0,0 +1,5 @@
+from recenzor.app import flask_app
+if __name__ == "__main__":
+    debug = True
+    host = "0.0.0.0"
+    flask_app.run(host, debug=debug)
